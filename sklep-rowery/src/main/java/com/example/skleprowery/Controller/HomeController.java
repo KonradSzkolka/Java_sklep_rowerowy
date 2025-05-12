dfeb550371dffa7de866d7832d17e5f3ef745e66
Konrad <115174801+KonradSzkolka@users.noreply.github.com>
2025-05-12 14:03:14 +0200

    to żyje

--- sklep-rowery/src/main/java/com/example/skleprowery/Controller/HomeController.java
+++ sklep-rowery/src/main/java/com/example/skleprowery/Controller/HomeController.java
@@ -2,14 +2,19 @@ package com.example.skleprowery.Controller;
 
 import com.example.skleprowery.Model.Item;
 import com.example.skleprowery.repository.ItemRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.swing.text.html.StyleSheet;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -26,5 +31,16 @@ public class HomeController {
         //List<Item> items = itemRepository.findAll();
         model.addAttribute("items", itemRepository.findAll());
         return "home";
+    }
+    @GetMapping("/add/{itemId}")
+    public String addItemToCart(@PathVariable("itemId") Long itemId, HttpSession session) {
+        List<Item> cart = (List<Item>) session.getAttribute("cart");
+        if (cart == null) {
+            cart = new ArrayList<>();
+        }
+        Optional<Item> oItem = itemRepository.findById(itemId);
+        oItem.ifPresent(cart::add);
+        session.setAttribute("cart", cart);
+        return "redirect:/";
     }
 }
--- sklep-rowery/src/main/java/com/example/skleprowery/DbInit.java
+++ sklep-rowery/src/main/java/com/example/skleprowery/DbInit.java
@@ -21,7 +21,7 @@ public class DbInit implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         itemRepository.saveAll(List.of(
-                new Item("https://m-bike.pl/wp-content/uploads/2025/04/MY23_M_Bike_GRV_100_Gray_Blue-9.jpg",new BigDecimal("200.20"),"Rower"),
+                new Item("https://i.pinimg.com/originals/bc/2b/dd/bc2bdd2d0eb20fa5668a75c778b1bfba.gif",new BigDecimal("200.20"),"Rower"),
         new Item("https://marketrowerowy.pl/images/5.1.png",new BigDecimal("1000.50"),"Goral"),
         new Item("https://dostawanajutro.pl/environment/cache/images/0_0_productGfx_15269/rower-MIFA-3-biegi_mietowy_4a.jpg",new BigDecimal("100.00"),"Skladak")
                 ));

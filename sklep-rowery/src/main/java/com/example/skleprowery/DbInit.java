a9de8dc51064d2a7757532a1862f57aba58b32f9
Konrad <115174801+KonradSzkolka@users.noreply.github.com>
2025-06-04 20:50:44 +0200

    2137

--- sklep-rowery/src/main/java/com/example/skleprowery/DbInit.java
+++ sklep-rowery/src/main/java/com/example/skleprowery/DbInit.java
@@ -10,20 +10,19 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @Configuration
+
 public class DbInit implements CommandLineRunner {
     private final ItemRepository itemRepository;
-
     @Autowired
     public DbInit(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
-
     }
     @Override
     public void run(String... args) throws Exception {
         itemRepository.saveAll(List.of(
-                new Item("https://i.pinimg.com/originals/bc/2b/dd/bc2bdd2d0eb20fa5668a75c778b1bfba.gif",new BigDecimal("200.29"),"Rower"),
-        new Item("https://marketrowerowy.pl/images/5.1.png",new BigDecimal("1000.51"),"Goral"),
-        new Item("https://dostawanajutro.pl/environment/cache/images/0_0_productGfx_15269/rower-MIFA-3-biegi_mietowy_4a.jpg",new BigDecimal("100.99"),"Skladak")
-                ));
+                new Item("SPECJALNY", new BigDecimal(999.99), "https://i.pinimg.com/originals/bc/2b/dd/bc2bdd2d0eb20fa5668a75c778b1bfba.gif"),
+                new Item("GÓRSKI", new BigDecimal(1500), "https://marketrowerowy.pl/images/5.1.png"),
+                new Item("SKŁADAK", new BigDecimal(250.55), "https://dostawanajutro.pl/environment/cache/images/0_0_productGfx_15269/rower-MIFA-3-biegi_mietowy_4a.jpg")
+        ));
     }
 }
